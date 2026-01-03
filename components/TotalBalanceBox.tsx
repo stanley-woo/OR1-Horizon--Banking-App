33c02fcb1a8280b484eea5addd516e5b68249811
Stanley Woo <46428542+stanley-woo@users.noreply.github.com>
2026-01-02 18:45:24 -0800

    Starting sidebars. Jan 2 Fri.

---
+++ components/TotalBalanceBox.tsx
@@ -0,0 +1,33 @@
+import { formatAmount } from '@/lib/utils'
+import React from 'react'
+import DoughnutChart from './ui/DoughnutChart'
+
+const TotalBalanceBox = ({
+    accounts = [], totalBanks, totalCurrentBalance
+}: TotlaBalanceBoxProps) => {
+  return (
+    <section className="total-balance">
+        TotalBalanceBox
+        <div className="total-balance-chart">
+            <DoughnutChart accounts={accounts} />
+        </div>
+        
+        <div className="flex flex-col gap-6">
+            <h2 className="header-2">
+                Bank Accounts: {totalBanks}
+            </h2>
+            <div className="flex flex-col gap-2">
+                <p className="total-balance-label">
+                    Total Current Balance
+                </p>
+
+                <p className="total-balance-amount flex-center gap-2">
+                    {formatAmount(totalCurrentBalance)}
+                </p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default TotalBalanceBox
